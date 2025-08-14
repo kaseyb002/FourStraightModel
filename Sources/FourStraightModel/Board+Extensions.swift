5ee19e35925a6e881142f04ad1e237cb0554db73
Kasey Baughan <kaseyb002@yahoo.com>
2025-08-13 21:30:27 -0400

    winning positions

--- Sources/FourStraightModel/Board+Extensions.swift
+++ Sources/FourStraightModel/Board+Extensions.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 extension [[Player?]] {
-    public static func emptyBoard(rows: Int, columns: Int) -> [[Player?]] {
+    public static func emptyBoard(
+        rows: Int = 6,
+        columns: Int = 7
+    ) -> [[Player?]] {
         let rowSpots: Array<Player?> = .init(repeating: nil, count: columns)
         return Array(repeating: rowSpots, count: rows)
     }
