a1c7ce204792555d503915798517ace20db6a5f6
Kasey Baughan <kaseyb002@yahoo.com>
2025-09-05 21:39:49 -0400

    extension

--- Sources/FourStraightModel/Board+Extensions.swift
+++ Sources/FourStraightModel/Board+Extensions.swift
@@ -8,4 +8,13 @@ extension [[Player?]] {
         let rowSpots: Array<Player?> = .init(repeating: nil, count: columns)
         return Array(repeating: rowSpots, count: rows)
     }
+    
+    public func isFilled(column: Int) -> Bool {
+        guard !self.isEmpty,
+              column >= 0,
+              column < self[0].count else {
+            return false
+        }
+        return self[0][column] != nil
+    }
 }
